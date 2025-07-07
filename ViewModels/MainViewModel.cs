70d8de8f3718468715946f9736678d4301bc2861
Simeon Arshyn <arshyn@gmail.com>
2025-07-07 09:15:58 +0200

    neue struktur aufgebaut.

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMMDemoApp.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private bool _isLeftPanelVisible = true;
+        public bool IsLeftPanelVisible
+        {
+            get => _isLeftPanelVisible;
+            set { _isLeftPanelVisible = value; OnPropertyChanged(nameof(IsLeftPanelVisible)); }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
--- Views/MainView.xaml.cs
+++ Views/MainView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using CMMDemoApp.ViewModels;
+using CMMDemoApp.Views.Panels;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace CMMDemoApp.Views
 {
@@ -7,9 +10,77 @@ namespace CMMDemoApp.Views
     /// </summary>
     public partial class MainView : Window
     {
+        public bool IsLeftPanelVisible { get; set; } = true;
+        public bool IsRightPanelVisible { get; set; } = true;
+        public bool Is3DViewVisible { get; set; } = true;
+
         public MainView()
         {
             InitializeComponent();
+            PanelSelector.SelectedIndex = 0;
+            this.DataContext = new MainViewModel();
+        }
+
+        private string _currentPanelTag = null;
+
+        /// <summary>
+        /// Вызывается при выборе вкладки в боковом ListBox
+        /// </summary>
+        private void PanelSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (PanelSelector.SelectedItem is ListBoxItem item)
+            {
+                string tag = item.Tag?.ToString();
+
+                // Показать левую колонку, если она была скрыта
+                if (LeftContentColumn.Width.Value == 0)
+                    LeftContentColumn.Width = new GridLength(250);
+
+                // Подключить верхнее окно
+                switch (tag)
+                {
+                    case "Features":
+                        LeftUpperPanel.Content = new FeaturesPanel(); break;
+                    case "Rules":
+                        LeftUpperPanel.Content = new RulesPanel(); break;
+                    case "Inspection":
+                        LeftUpperPanel.Content = new InspectionPanel(); break;
+                }
+
+                // Нижняя панель — всегда Property View
+                if (LeftLowerPanel.Content == null)
+                {
+                    LeftLowerPanel.Content = new PropertiesPanel();
+                }
+
+                _currentPanelTag = tag;
+            }
+        }
+
+        private void LeftPanelCheckBox_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            bool isChecked = (sender as CheckBox)?.IsChecked ?? false;
+
+            if (isChecked)
+            {
+                var item = PanelSelector.SelectedItem as ListBoxItem;
+                if (item == null)
+                {
+                    // Активируем первую вкладку, если ничего не выбрано
+                    PanelSelector.SelectedIndex = 0;
+                }
+                else
+                {
+                    LeftContentColumn.Width = new GridLength(250);
+                }
+            }
+            else
+            {
+                // Скрываем панель
+                LeftContentColumn.Width = new GridLength(0);
+                //PanelContent.Content = null;
+                _currentPanelTag = null;
+            }
         }
     }
 }
