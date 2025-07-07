8747b209208f7b9a0e1ec86e1969a3aebfd2592d
Simeon Arshyn <arshyn@gmail.com>
2025-07-07 13:09:03 +0200

    3D logo added

--- Views/Panels/WebView.xaml.cs
+++ Views/Panels/WebView.xaml.cs
@@ -19,7 +19,7 @@ namespace CMMDemoApp.Views.Panels
         private async void InitWebView()
         {
             await webView.EnsureCoreWebView2Async();
-            // webView.CoreWebView2.OpenDevToolsWindow();
+            webView.CoreWebView2.OpenDevToolsWindow();
 
             var editorPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Editor");
             if (!Directory.Exists(editorPath))
@@ -36,13 +36,6 @@ namespace CMMDemoApp.Views.Panels
             // Lädt index.html aus dem lokalen Ordner editor über den virtuellen Host
             webView.Source = new Uri("https://app/index.html");
 
-            // WebMessageReceived einmalig verbinden
-            //webView.CoreWebView2.WebMessageReceived += (sender, args) =>
-            //{
-            //    string message = args.TryGetWebMessageAsString();
-            //    File.WriteAllText("exported_model.json", message);
-            //    System.Windows.MessageBox.Show("Modell exportiert.");
-            //};
         }
     }
 }
